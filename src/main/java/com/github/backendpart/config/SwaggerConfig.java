51b820bb5fe25730909105e726530470cb5dabf1
JungWanSEO <36655724+JungWanSEO@users.noreply.github.com>
2024-01-21 17:02:26 +0900

    fix: swagger세팅 다시 수정 - info의 예시가 없었습니다.

--- src/main/java/com/github/backendpart/config/SwaggerConfig.java
+++ src/main/java/com/github/backendpart/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.github.backendpart.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -10,6 +11,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    Info info = new Info()
+            .title("Api 문서")
+            .description("잘못된 부분이나 오류 발생 시 바로 말씀해주세요")
+            .contact(new Contact().name("접근하지말아주세요"));
     @Bean
     public OpenAPI openAPI() {
         String jwtSchemeName = "jwtAuth";
