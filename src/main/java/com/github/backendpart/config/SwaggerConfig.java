e8cbcacc7e3c045b3cec5ba1722d346cc659d484
JungWanSEO <36655724+JungWanSEO@users.noreply.github.com>
2024-01-21 16:11:18 +0900

    fix: swagger세팅 수정 - jwt 인증관련 swagger 추가

--- src/main/java/com/github/backendpart/config/SwaggerConfig.java
+++ src/main/java/com/github/backendpart/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.github.backendpart.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +12,24 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-      return new OpenAPI()
-              .info(new Info()
-                      .title("프로젝트 게시판 관련 API")
-                      .description("Swagger ")
-                      .version("1.0.0"));
+        String jwtSchemeName = "jwtAuth";
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .name(jwtSchemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
+
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Authorization", bearerAuth))
+                .addSecurityItem(addSecurityItem)
+                .info(info);
     }
+
+
 }
