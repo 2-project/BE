619fc55a550776e431710016ae50de3d138f0264
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2024-01-23 21:26:28 +0900

    fix: 회원탈퇴 로직 수정
    암호화된 비밀번호와 유저가 입력한 비밀번호 비교 로직

--- src/main/java/com/github/backendpart/service/UnregisterService.java
+++ src/main/java/com/github/backendpart/service/UnregisterService.java
@@ -1,18 +1,14 @@
 package com.github.backendpart.service;
-
-
-import com.amazonaws.services.kms.model.NotFoundException;
 import com.github.backendpart.repository.AuthRepository;
 import com.github.backendpart.repository.CartRepository;
-import com.github.backendpart.repository.OrderRepository;
 import com.github.backendpart.repository.UserCartRepository;
 import com.github.backendpart.web.entity.CartEntity;
 import com.github.backendpart.web.entity.UserCartEntity;
 import com.github.backendpart.web.entity.users.UserEntity;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -21,15 +17,17 @@ public class UnregisterService {
     private AuthRepository authRepository;
     private UserCartRepository userCartRepository;
     private CartRepository cartRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void deleteUser(Long userId, String password) {
         UserEntity userEntity = authRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자를 찾을 수 없습니다."));
 
-        if (!userEntity.getUserPwd().equals(password)) {
+        if (!passwordEncoder.matches(password, userEntity.getUserPwd())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
+
 
         List<UserCartEntity> userCarts = userCartRepository.findByUser(userEntity);
         for (UserCartEntity userCart : userCarts) {
@@ -39,8 +37,6 @@ public class UnregisterService {
             }
             userCartRepository.delete(userCart);
         }
-
-        // 사용자 정보 삭제
         authRepository.delete(userEntity);
     }
 }
