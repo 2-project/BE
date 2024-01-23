333b35c158138ce5650758deac13ebc47b9056a9
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2024-01-23 17:08:54 +0900

    fix: getUserCid 추가
    <Cart/Order>
    - @AuthenticationPrincipal을 사용하여 CustomUserDetail의 유저id를 가져오는 로직을 추가했습니다.
    - deleteCart 메서드내 로직을 추가했습니다. (해당 user의 장바구니에 들어있는 물품만 삭제하도록)

--- src/main/java/com/github/backendpart/web/controller/CartController.java
+++ src/main/java/com/github/backendpart/web/controller/CartController.java
@@ -6,12 +6,15 @@ import com.github.backendpart.web.dto.cart.CartDto;
 import com.github.backendpart.web.dto.cart.ResultAddCartDto;
 import com.github.backendpart.web.dto.cart.UpdateCartDTO;
 import com.github.backendpart.web.dto.common.CommonResponseDto;
+import com.github.backendpart.web.entity.users.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -30,10 +33,9 @@ public class CartController {
 
     @Operation(summary = "장바구니 조회", description = "장바구니에 담긴 모든 물품을 조회한다")
     @GetMapping("")
-    public ResponseEntity<List<CartDto>> viewCart() {
-        // 토큰 가져와서 findAllCart에 넘겨주기
+    public ResponseEntity<List<CartDto>> viewCart(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         log.info("[GET] 장바구니 조회 요청이 들어왔습니다.");
-        List<CartDto> allCart = cartService.findAllCart();
+        List<CartDto> allCart = cartService.findAllCart(customUserDetails);
         return ResponseEntity.ok().body(allCart);
     }
 
@@ -42,27 +44,35 @@ public class CartController {
     @PostMapping("/{productId}")
     public ResponseEntity<ResultAddCartDto> addCart(
             @PathVariable(name ="productId") Long productId,
-            @RequestBody AddCartDto addCartDTO) {
+            @RequestBody AddCartDto addCartDTO,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         log.info("[POST] 장바구니 상품 추가 요청이 들어왔습니다.");
-        ResultAddCartDto resultAddCart = cartService.addCart(productId,addCartDTO);
+        if (customUserDetails == null) {
+            log.error("customUserDetails is null!");
+            // 추가적인 디버깅 정보 또는 예외 처리를 추가할 수 있습니다.
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        ResultAddCartDto resultAddCart = cartService.addCart(productId,addCartDTO,customUserDetails);
         return ResponseEntity.ok().body(resultAddCart);
 
     }
 
     @Operation(summary = "장바구니 삭제",description = "장바구니 목록에서 특정물품을 삭제한다")
     @DeleteMapping("/{productId}")
-    public CommonResponseDto deleteCart(@PathVariable(name ="productId") Long productId) {
+    public CommonResponseDto deleteCart(@PathVariable(name ="productId") Long productId,
+                                        @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         log.info("[DELETE] 장바구니 물품 삭제 요청이 들어왔습니다.");
-        return cartService.deleteCart(productId);
+        return cartService.deleteCart(productId,customUserDetails);
     }
 
     @Operation(summary = "장바구니 수정",description = "장바구니 목록의 특정물품 수량 또는 옵션을 수정한다")
     @PutMapping("/{productId}") // 또는 cartId
     public CommonResponseDto updateCart(
             @PathVariable(name ="productId") Long productId,
-            @RequestBody UpdateCartDTO updateCartDTO){
+            @RequestBody UpdateCartDTO updateCartDTO,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails){
         log.info("[PUT] 장바구니 물품 옵션/수량 변경 요청이 들어왔습니다.");
-        return cartService.updateCart(productId,updateCartDTO);
+        return cartService.updateCart(productId,updateCartDTO,customUserDetails);
 
     }
 }
--- src/main/java/com/github/backendpart/web/controller/OrderController.java
+++ src/main/java/com/github/backendpart/web/controller/OrderController.java
@@ -4,11 +4,13 @@ import com.github.backendpart.service.OrderService;
 import com.github.backendpart.web.dto.common.CommonResponseDto;
 import com.github.backendpart.web.dto.order.OrderDto;
 import com.github.backendpart.web.dto.order.PayInfoDto;
+import com.github.backendpart.web.entity.users.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,9 +22,9 @@ public class OrderController {
     private final OrderService orderService;
     @Operation(summary = "주문하기", description = "장바구니에 담긴 물품들을 주문한다.")
     @PostMapping("")
-    public ResponseEntity<OrderDto> orderCart(){
+    public ResponseEntity<OrderDto> orderCart(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         log.info("[POST] 장바구니 상품 목록 주문 요청 들어왔습니다.");
-        OrderDto orderDto = orderService.orderCart();
+        OrderDto orderDto = orderService.orderCart(customUserDetails);
         return ResponseEntity.ok().body(orderDto);
     }
 
